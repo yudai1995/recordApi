639030f33cd173ba1f8db6996ea92aa936b3fd00
yamamoto <y.yamamoto991103@gmail.com>
2024-10-06 21:04:50 +0900

    chore: prettier, eslint, debug, dbの設定ファイルを変更

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -1,14 +1,14 @@
 import { DataSource } from 'typeorm';
 
 export default new DataSource({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  database: 'test',
-  username: 'test',
-  password: 'password',
-  entities: ['dist/**/entities/**/*.entity.js'],
-  migrations: ['dist/**/migrations/**/*.js'],
-  logging: true,
-  //   synchronize: true
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    database: 'recorder',
+    username: 'Admin',
+    password: 'password',
+    entities: ['dist/**/entities/**/*.entity.js'],
+    migrations: ['dist/**/migrations/**/*.js'],
+    logging: true,
+    // synchronize: false,
 });
