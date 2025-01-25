9dd4bd7e3678889a31d430691a501007bb6952ae
yamamoto <y.yamamoto991103@gmail.com>
2025-01-25 12:35:51 +0900

    feat: packpage update

--- main/index.ts
+++ main/index.ts
@@ -1,7 +1,6 @@
-import { HttpRequest } from '@azure/functions';
+import { Context, HttpRequest } from '@azure/functions';
 import { AzureHttpAdapter } from '@nestjs/azure-func-http';
 import { createApp } from '../src/main.azure';
-import { Context } from 'vm';
 
 export default function (context: Context, req: HttpRequest): void {
     AzureHttpAdapter.handle(createApp, context, req);
